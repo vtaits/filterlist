a9b58bd25824d16ab752fcf84b67d80289d4af59
Vadim Taits <taizvadim@gmail.com>
2024-01-28 12:38:33 +0300

    Upgrade yarn version

--- packages/react-filterlist/src/defaultShouldRecount.ts
+++ packages/react-filterlist/src/defaultShouldRecount.ts
@@ -1,4 +1,4 @@
-export const defaultShouldRecount = <FiltersAndSortData,>(
+export const defaultShouldRecount = <FiltersAndSortData>(
 	data1: FiltersAndSortData,
 	data2: FiltersAndSortData,
 ): boolean => data1 === data2;
