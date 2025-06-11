19c7df39660a0c7c5cfecba3b44db3ca80872158
Vadim Taits <taizvadim@gmail.com>
2025-06-11 16:01:57 +0300

    Remove CommonJS build from packages

--- packages/react-filterlist-router/src/useCreateDataStore.test.tsx
+++ packages/react-filterlist-router/src/useCreateDataStore.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, mock, test } from "bun:test";
 import { renderHook, waitFor } from "@testing-library/react";
-import type { StringBasedDataStoreOptions } from "@vtaits/filterlist/dist/datastore_string";
+import type { StringBasedDataStoreOptions } from "@vtaits/filterlist/datastore/string";
 import { type Params, useFilterlist } from "@vtaits/react-filterlist";
 import { type PropsWithChildren, type RefObject, createRef } from "react";
 import {
